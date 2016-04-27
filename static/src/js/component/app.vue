4d548c59e416a8414401f71d0c9d395ef094b381
luoye <842891024@qq.com>
2016-04-27 21:13:03 +0800

    install package...

--- static/src/js/component/app.vue
+++ static/src/js/component/app.vue
@@ -1,6 +1,14 @@
 <template>
+	<div class="main_bg"></div>
 	<h1>首页</h1>
 </template>
+
+
+<style type="text/css">
+	
+.main_bg{position: fixed;top: 0;left: 0;width: 100%;height: 100%;background: #ff5151;}
+
+</style>
 
 <script lang="babel">
 
