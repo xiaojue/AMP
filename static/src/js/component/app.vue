04fe0600464bb375c21a7eb5accebe5e523acbf9
luoye <842891024@qq.com>
2016-04-27 21:16:32 +0800

    index style

--- static/src/js/component/app.vue
+++ static/src/js/component/app.vue
@@ -6,7 +6,7 @@
 
 <style type="text/css">
 	
-.main_bg{position: fixed;top: 0;left: 0;width: 100%;height: 100%;background: #ff5151;}
+.main_bg{position: fixed;top: 0;left: 0;width: 100%;height: 100%;background: #ff5151;z-index: -1;}
 
 </style>
 
