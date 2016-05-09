5556ca765ac272027c6b47521c31eb78e1eacb4d
luoye <842891024@qq.com>
2016-05-09 23:54:12 +0800

    add debug method for vuex

--- static/src/js/debug.js
+++ static/src/js/debug.js
@@ -1,1 +1,10 @@
-console.log('debug');
+/*
+ * just for debug
+ * will delete when env=pro
+ */
+
+import store from 'store';
+import actions from 'actions';
+
+window.store = store;
+window.actions = actions;
