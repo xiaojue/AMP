467fc7d3607f5f46aa1993fd697b29cde2e4690c
luoye <842891024@qq.com>
2016-05-13 17:48:51 +0800

    response

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,19 @@
-// export const resopnse
+export const response = async (ctx, next) => {
+	ctx.success = (result, msg)=>{
+	    ctx.body = {
+	        code: 200,
+	        data: result,
+	        iserror: 0,
+	        msg: ''
+	    }
+	}
+	ctx.fail = (code, msg)=>{
+	    ctx.body = {
+	        code: code,
+	        data: '',
+	        iserror: 1,
+	        msg: msg
+	    }
+	}
+	await next();
+}
