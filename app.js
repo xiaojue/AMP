faec5eeb61b48bef1b20c5f6aa2cdeb52c385b94
luoye <842891024@qq.com>
2016-05-13 13:41:43 +0800

    mongodb

--- app.js
+++ app.js
@@ -3,54 +3,45 @@
  * AMP 入口文件
  */
 
-import path from 'path';
-
-import Koa from 'koa';
-import Router from 'koa-router';
-import Static from 'koa-static';
-import Logger from 'koa-logger';
-import Send from 'koa-send';
-import KoaBodyParser  from 'koa-better-body';
 import fs from 'fs';
+import path from 'path';
 import https from 'https';
 
-import session from 'koa2-cookie-session';
+import Koa from 'koa';
+import Static from 'koa-static';
+import Logger from 'koa-logger';
+import KoaBodyParser from 'koa-better-body';
+import Session from 'koa2-cookie-session';
+
+import routers from './routers';
 
 // koa1中间件转换
 import convert from 'koa-convert';
-
-import routers from './routes';
-import db from './config/db.json';
-import mysqlMiddleware from './middleware/mysql';
-import responseMiddleware from './middleware/response'
 
 const app = new Koa();
 const httpPort = 9090;
 const httpsPort = 8989;
 const options = {
-    key: fs.readFileSync('./privatekey.pem'),
-    cert: fs.readFileSync('./certificate.pem')
+    key: fs.readFileSync('./pem/privatekey.pem'),
+    cert: fs.readFileSync('./pem/certificate.pem')
 };
 
 
 app.use(KoaBodyParser());
-app.use(session());
+app.use(Session());
 
 
 // middleware
-app.use(mysqlMiddleware);
-app.use(responseMiddleware);
-
 app.use(convert(Logger()));
 
+// static
 app.use(convert(Static(path.join(__dirname, 'static'))));
 
-
-for(let item of routers){
-    app.use(item.routes(),item.allowedMethods());
+for (let item of routers) {
+    app.use(item.routes(), item.allowedMethods());
 }
 
-// 错误处理
+// err handler
 app.on('error', (err, ctx) => {
     console.error('server error', err, ctx);
 });
