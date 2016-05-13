91a14d7c2d2d28e2c8da2a1c4a0a111b37f1ef45
luoye <842891024@qq.com>
2016-05-13 17:12:53 +0800

    model

--- database/model.js
+++ database/model.js
@@ -1,10 +1,30 @@
 export default {
     users: {
-        user_name: { type: String, required: true },
-
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        department: { type: String, required: true },
+        role: { type: String, required: true }
     },
     projects: {
-
+        name: { type: String, required: true },
+        desc: { type: String, required: true },
+        creator: { type: String, required: true },
+        creat_time: { type: Number, required: true },
+        members: { type: Array, required: true },
+        apis: { type: Array, required: true },
     },
-    
+    urls: {
+        name: { type: String, required: true },
+        desc: { type: String, required: true },
+        creator: { type: String, required: true },
+        creat_time: { type: Number, required: true },
+        url: { type: String, required: true },
+        method: { type: String, required: true },
+        request_params: { type: Object, required: true }, // 请求参数说明
+        request_example: { type: Object, required: true }, // 请求示例
+        // response_type: { type: String, required: true }, // json/xml/html etc.
+        response_params: { type: Object, required: true }, // 返回的参数说明
+        response_example: { type: Object, required: true }, // 返回示例
+        remark: { type: String, required: true }, // 备注
+    }    
 }
