cf501d031155b66e896c8725a6bd4dd4e6eb9fb0
luoye <842891024@qq.com>
2016-05-19 14:49:39 +0800

    mock

--- routers/mock.js
+++ routers/mock.js
@@ -9,11 +9,11 @@ const Mock = Router({
 });
 
 // 非登陆接口全局需验证
-Mock.get('/:project_id/' + api_url, async (ctx, next) => {
+Mock.get('/:project_id/:api_url', async (ctx, next) => {
 	// console.log(api_url);
 	console.log(ctx.params.project_id);
 	console.log(ctx.params.api_url);
-	ctx.success('success')
+	ctx.success('success');
 });
 
 export default Mock;
