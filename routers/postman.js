f2179f26a58d79c85cd48a74da728e3637a38a7e
luoye <842891024@qq.com>
2016-05-19 23:16:27 +0800

    postman

--- routers/postman.js
+++ routers/postman.js
@@ -8,40 +8,18 @@ import request from 'request';
 
 const Postman = Router();
 
-// const requestPromise = () => {
-// 	return new Promise((resolve, reject) => {
-// 		request = resolve;
-// 	})
-// }
 
-
-// 非登陆接口全局需验证
 Postman.post('/postman', async (ctx, next) => {
     const obj = ctx.body;
 
-
-
-    // await requestPromise({
-    // 	url: obj.url,
-    // 	method: obj.type
-    // }, (err, res, body) => {
-    // 	console.log(body);
-    // 	console.log(1);
-    	
-    // })
-
-	ctx.success('123', '成功');
-
-    // ctx.body 
-    // .on('error', (err) => {
-    // 	console.log(err);
-    // })
-    // .on('end', () => {
-
-    // })
-    // console.log(2);
-    // ctx.success(res, '成功');
-
+    await new Promise((resolve, reject) => {
+        request({
+            url: obj.url,
+            method: obj.type
+        },(err, res, body) => {
+            ctx.success(body, '成功');
+        })
+    })
 });
 
 export default Postman;
