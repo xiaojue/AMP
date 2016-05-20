6572d25ee3e2d2b260324cc4aebdcd006572eb5a
luoye <842891024@qq.com>
2016-05-20 16:57:38 +0800

    postman update

--- routers/postman.js
+++ routers/postman.js
@@ -12,10 +12,23 @@ const Postman = Router();
 Postman.post('/postman', async (ctx, next) => {
     const obj = ctx.body;
 
+    const params = ctx.body.params;
+
+    if(obj.type == 'get' || obj.type == 'delete'){
+        obj.url = obj.url + '?'
+
+        let _curr = [];
+        Object.keys(params).forEach((item) => {
+            _curr.push(item + '=' + params[item]);
+        })
+        obj.url += _curr.join('&');
+    }
+
     await new Promise((resolve, reject) => {
         request({
             url: obj.url,
-            method: obj.type
+            method: obj.type,
+            form: params
         },(err, res, body) => {
         	ctx.body = body;
         	resolve();
