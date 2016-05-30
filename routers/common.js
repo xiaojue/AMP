eaf759e5614f314707430873b4f8908d2b46a818
luoye <842891024@qq.com>
2016-05-30 13:17:22 +0800

    ignore mock api authority check

--- routers/common.js
+++ routers/common.js
@@ -6,8 +6,8 @@ import Router from 'koa-router';
 
 const Common = Router();
 
-// 非登陆接口全局需验证
-Common.all(/^((?!\/user).)*$/, async (ctx, next) => {
+// 无需登陆接口过滤
+Common.all(/^((?!\/user|mock).)*$/, async (ctx, next) => {
     if(!ctx.session.isLogin){
         ctx.fail(401,'请登录');
         return;
